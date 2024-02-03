a38d60c6866c7db72850078d219cf41e9453887a
IVANWARS <IVANWARS230@yandex.ru>
2024-02-04 01:07:32 +0300

    нахождение числа в массиве

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = { 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+int numberForSearch = 44;
+bool isFound = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (numberForSearch == array[i])
+    {
+        isFound = true;
+        //Console.WriteLine("да");
+        break;
+    }
+
+}
+if (isFound)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
